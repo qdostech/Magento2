c25d1f7a88dcb1c89cd6a259948223651e675db4
Ravi Mule <ravi.mule@wwindia.com>
2020-04-23 13:12:27 +0530

    Bug Fixes

--- app/code/Neo/Storemapping/Controller/Adminhtml/Storemapping/Save.php
+++ app/code/Neo/Storemapping/Controller/Adminhtml/Storemapping/Save.php
@@ -1,27 +1,24 @@
 <?php
-
 namespace Neo\Storemapping\Controller\Adminhtml\Storemapping;
-
 use Magento\Framework\App\Filesystem\DirectoryList;
-
 class Save extends \Magento\Backend\App\Action
 {
     /**
      * @var \Magento\Framework\View\Result\PageFactory
      */
-    public function execute()
+	public function execute()
     {
-
+		
         $data = $this->getRequest()->getParams();
         if ($data) {
             $model = $this->_objectManager->create('Neo\Storemapping\Model\Storemapping');
-            $id = $this->getRequest()->getParam('id');
+			$id = $this->getRequest()->getParam('id');
             if ($id) {
                 $model->load($id);
             }
-
+			
             $model->setData($data);
-
+			
             try {
                 $model->save();
                 $this->messageManager->addSuccess(__('Storemapping Has been Saved.'));
